d68f389927b0f4d91ecef31c82a9d6d7f75d9837
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-11-13 13:43:14 +0100

    Add open graph and twitter meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,6 +60,26 @@
     }
 </script>
 
+<svelte:head>
+    <title>Ontolo</title>
+    <meta name="description" content="Ontolo is a fun tool to label Nostr events." />
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://www.ontolo.social" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Ontolo" />
+    <meta property="og:description" content="Ontolo is a fun tool to label Nostr events." />
+    <meta property="og:image" content="https://i.nostr.build/neVn.webp" />
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:domain" content="www.ontolo.social" />
+    <meta property="twitter:url" content="https://www.ontolo.social" />
+    <meta name="twitter:title" content="Ontolo" />
+    <meta name="twitter:description" content="Ontolo is a fun tool to label Nostr events." />
+    <meta name="twitter:image" content="https://i.nostr.build/neVn.webp" />
+</svelte:head>
+
 <div class="eventContainer px-4 lg:px-0 flex flex-col lg:flex-row lg:justify-between lg:gap-6">
     {#if event}
         <EventCard {event} />
